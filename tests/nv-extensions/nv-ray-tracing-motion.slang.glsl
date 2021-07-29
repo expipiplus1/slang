091b161e11a5ecddbfcbc1d9e5fd010762bd7a74
jsmall-nvidia <jsmall@nvidia.com>
2021-07-29 15:05:27 -0400

    Fix updated glslang output for nv-ray-tracing-motion.slang.glsl

--- tests/nv-extensions/nv-ray-tracing-motion.slang.glsl
+++ tests/nv-extensions/nv-ray-tracing-motion.slang.glsl
@@ -41,7 +41,8 @@ struct ShadowRay_0
 };
 
 layout(location = 0)
-rayPayloadEXT ShadowRay_0 p_0;
+rayPayloadEXT
+ShadowRay_0 p_0;
 
 struct RayDesc_0
 {
@@ -53,9 +54,13 @@ struct RayDesc_0
 
 void TraceMotionRay_0(accelerationStructureEXT AccelerationStructure_0, uint RayFlags_0, uint InstanceInclusionMask_0, uint RayContributionToHitGroupIndex_0, uint MultiplierForGeometryContributionToHitGroupIndex_0, uint MissShaderIndex_0, RayDesc_0 Ray_0, float CurrentTime_0, inout ShadowRay_0 Payload_0)
 {
+
     p_0 = Payload_0;
+
     traceMotionNV(AccelerationStructure_0, RayFlags_0, InstanceInclusionMask_0, RayContributionToHitGroupIndex_0, MultiplierForGeometryContributionToHitGroupIndex_0, MissShaderIndex_0, Ray_0.Origin_0, Ray_0.TMin_0, Ray_0.Direction_0, Ray_0.TMax_0, CurrentTime_0, (0));
+
     Payload_0 = p_0;
+
     return;
 }
 
@@ -64,7 +69,9 @@ uniform accelerationStructureEXT as_0;
 
 float saturate_0(float x_0)
 {
+
     float _S2 = clamp(x_0, float(0), float(1));
+
     return _S2;
 }
 
@@ -74,13 +81,18 @@ struct ReflectionRay_0
 };
 
 layout(location = 1)
-rayPayloadEXT ReflectionRay_0 p_1;
+rayPayloadEXT
+ReflectionRay_0 p_1;
 
 void TraceRay_0(accelerationStructureEXT AccelerationStructure_1, uint RayFlags_1, uint InstanceInclusionMask_1, uint RayContributionToHitGroupIndex_1, uint MultiplierForGeometryContributionToHitGroupIndex_1, uint MissShaderIndex_1, RayDesc_0 Ray_1, inout ReflectionRay_0 Payload_1)
 {
+
     p_1 = Payload_1;
+
     traceRayEXT(AccelerationStructure_1, RayFlags_1, InstanceInclusionMask_1, RayContributionToHitGroupIndex_1, MultiplierForGeometryContributionToHitGroupIndex_1, MissShaderIndex_1, Ray_1.Origin_0, Ray_1.TMin_0, Ray_1.Direction_0, Ray_1.TMax_0, (1));
+
     Payload_1 = p_1;
+
     return;
 }
 
@@ -125,65 +137,31 @@ void main()
     ShadowRay_0 shadowRay_0;
     shadowRay_0.hitDistance_0 = float(0);
 
-    const float currentTime_0 = float(1);
-
-    const uint _S10 = uint(1);
-
-    const uint _S11 = uint(255);
-
-    const uint _S12 = uint(0);
-
-    const uint _S13 = uint(0);
-
-    const uint _S14 = uint(2);
-
-    RayDesc_0 _S15 = ray_0;
-
-    ShadowRay_0 _S16;
-
-    _S16 = shadowRay_0;
-
-    TraceMotionRay_0(as_0, _S10, _S11, _S12, _S13, _S14, _S15, currentTime_0, _S16);
-
-    shadowRay_0 = _S16;
+    TraceMotionRay_0(as_0, uint(1), uint(255), uint(0), uint(0), uint(2), ray_0, float(1), shadowRay_0);
 
     if(shadowRay_0.hitDistance_0 < lightDist_0)
     {
+
         atten_0 = 0.00000000000000000000;
     }
     else
     {
+
         atten_0 = _S9;
     }
 
-    vec3 _S17 = ubo_0._data.light_0.color_0.xyz;
+    vec3 _S10 = ubo_0._data.light_0.color_0.xyz;
 
-    float _S18 = dot(N_0, L_0);
+    float _S11 = dot(N_0, L_0);
 
-    float _S19 = saturate_0(_S18);
+    float _S12 = saturate_0(_S11);
 
-    vec3 color_2 = _S17 * _S19 * atten_0;
+    vec3 color_2 = _S10 * _S12 * atten_0;
 
-    const uint _S20 = uint(1);
-
-    const uint _S21 = uint(255);
-
-    const uint _S22 = uint(0);
-
-    const uint _S23 = uint(0);
-
-    const uint _S24 = uint(2);
-
-    RayDesc_0 _S25 = ray_0;
-
-    ReflectionRay_0 _S26;
     ReflectionRay_0 reflectionRay_0;
+    TraceRay_0(as_0, uint(1), uint(255), uint(0), uint(0), uint(2), ray_0, reflectionRay_0);
 
-    _S26 = reflectionRay_0;
-
-    TraceRay_0(as_0, _S20, _S21, _S22, _S23, _S24, _S25, _S26);
-    imageStore((outputImage_0), ivec2((uvec2(launchID_0))), vec4(color_2 + _S26.color_1, 1.00000000000000000000));
-
+    imageStore((outputImage_0), ivec2((uvec2(launchID_0))), vec4(color_2 + reflectionRay_0.color_1, 1.00000000000000000000));
     return;
 }
 
