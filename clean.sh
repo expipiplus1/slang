1a648b75d318c856d9c90c1920c07662d9c96d39
Ellie Hermaszewska <ellieh@nvidia.com>
2023-04-27 10:26:18 +0800

    More files not to clean

--- clean.sh
+++ clean.sh
@@ -8,10 +8,14 @@ git clean \
   --exclude=.clang-format \
   --exclude=gdb-pretty.py \
   --exclude=tests \
-  --exclude=curses.patch \
+  --exclude=hidden-llvm.patch \
   --exclude=clean.sh \
   --exclude=compile_flags.txt \
+  --exclude=compile_commands.json \
+  --exclude=.cache \
   --exclude=.vscode \
   --exclude=.mailmap \
   --exclude=.gdb_history \
+  --exclude=vkd3d-proton.cache \
+  --exclude=.ignore \
   "$@"
