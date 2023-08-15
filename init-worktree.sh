3796329a57ee189900e4a8ea946c276bc16e12f2
Ellie Hermaszewska <ellieh@nvidia.com>
2023-08-15 11:02:54 +0800

    inline link script

--- init-worktree.sh
+++ init-worktree.sh
@@ -2,6 +2,15 @@
 
 script_dir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
-"$script_dir"/link-slang-utils.sh
+them=$(realpath --relative-to="$(pwd)" "$script_dir")
+
+for f in clean.sh .ignore .vscode; do
+  ln -v -s "$them/$f" "./$f"
+done
+
+for f in flake.nix flake.lock; do
+  cp -v "$them/$f" "./$f"
+done
+
 git submodule update --init --recursive
 nix-shell --run gen-compile-commands
