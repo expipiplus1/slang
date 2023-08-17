df4f3f41c13233e727b2ff27b6d7fd032c9d0dab
Ellie Hermaszewska <ellieh@nvidia.com>
2023-08-17 14:21:43 +0800

    set up direnv in init-worktree.sh

--- init-worktree.sh
+++ init-worktree.sh
@@ -2,15 +2,22 @@
 
 script_dir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
-them=$(realpath --relative-to="$(pwd)" "$script_dir")
+from=$(realpath --relative-to="$(pwd)" "$script_dir")
 
-for f in clean.sh .ignore .vscode; do
-  ln -v -s "$them/$f" "./$f"
+for f in .envrc clean.sh .ignore .vscode; do
+  ln -v -s "$from/$f" "./$f"
 done
 
-for f in flake.nix flake.lock; do
-  cp -v "$them/$f" "./$f"
+# Init direnv
+direnv allow
+
+# Update the submodules from something locally
+for f in $(grep '^\[submodule "\K.*(?="\]$)' .gitmodules --only-matching --perl-regexp); do
+  git submodule update --init --reference "$from/$f" --recursive -- "$f"
 done
 
+# Grab any missing ones
 git submodule update --init --recursive
-nix-shell --run gen-compile-commands
+
+# Generate our compilation command database
+nix develop "$from" --command gen-compile-commands
